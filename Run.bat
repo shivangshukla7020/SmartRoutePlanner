08df3507cc02f288dd6afe7bdc7f12a15f7356aa
shivangshukla7020 <shivangshukla201@gmail.com>
2025-06-03 18:01:53 +0530

    added refer.txt

--- Run.bat
+++ Run.bat
@@ -1,10 +1,21 @@
 @echo off
+cd /d "%~dp0"
+
 echo Cleaning and building the project...
 mvn clean install
 if errorlevel 1 (
     echo Build failed. Exiting.
+    pause
     exit /b 1
 )
+
 echo Running the project...
-mvn exec:java -Dexec.mainClass=com.example.smartroute.Main
+mvn exec:java -Dexec.mainClass=smartroute.Main
+if errorlevel 1 (
+    echo Run failed. Exiting.
+    pause
+    exit /b 1
+)
+
+echo Done!
 pause
